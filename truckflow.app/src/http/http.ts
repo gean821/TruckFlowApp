6876e1ff6b718fce75cd5682ff81380605773875
Felipe Tozadori Silva <ftozadori@gmail.com>
2026-04-11 17:15:43 -0300

    -fix(Ajuste .env): Ajustado .env

--- truckflow.app/src/http/http.ts
+++ truckflow.app/src/http/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const http = axios.create(
   {
-    baseURL: 'import.meta.env.VITE_API_UR',
+    baseURL: 'import.meta.env.VITE_API_URL',
     headers: { 'X-Custom-Header': 'foobar' }
   });
 
